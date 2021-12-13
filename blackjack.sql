e96e0e57408b66ffd2797bd953b04df8565b83a7
agunia2000 <gunia.amadeusz@gmaill.com>
2021-12-13 21:29:17 +0100

    database update

--- blackjack.sql
+++ blackjack.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 08 Gru 2021, 16:53
+-- Czas generowania: 13 Gru 2021, 21:28
 -- Wersja serwera: 10.4.21-MariaDB
 -- Wersja PHP: 8.0.12
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `blackjack`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `achievements`
+--
+
+CREATE TABLE `achievements` (
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `criterion` text NOT NULL,
+  `threshold` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `achievements`
+--
+
+INSERT INTO `achievements` (`name`, `description`, `criterion`, `threshold`) VALUES
+('5 wins', 'You have to win minimum 5 games.', 'wins', 5),
+('Snake Eyes', 'You have to ...', 'snakeEyes', 3),
+('21 points', 'no desc', 'blackjacks', 10);
 
 -- --------------------------------------------------------
 
@@ -61,7 +83,33 @@ CREATE TABLE `skins` (
 INSERT INTO `skins` (`login`, `sid`) VALUES
 ('admin', 3),
 ('admin', 2),
-('admin', 1);
+('admin', 1),
+('user', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `statistics`
+--
+
+CREATE TABLE `statistics` (
+  `login` text NOT NULL,
+  `wins` int(11) NOT NULL,
+  `failures` int(11) NOT NULL,
+  `blackjacks` int(11) NOT NULL,
+  `snakeEyes` int(11) NOT NULL,
+  `hardBotWins` int(11) NOT NULL,
+  `points` int(11) NOT NULL,
+  `revenue` int(11) NOT NULL,
+  `money` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `statistics`
+--
+
+INSERT INTO `statistics` (`login`, `wins`, `failures`, `blackjacks`, `snakeEyes`, `hardBotWins`, `points`, `revenue`, `money`) VALUES
+('admin', 7, 1, 0, 2, 3, 150, 200, 750);
 
 -- --------------------------------------------------------
 
@@ -73,7 +121,6 @@ CREATE TABLE `users` (
   `uid` int(10) UNSIGNED NOT NULL,
   `login` text NOT NULL,
   `password` text NOT NULL,
-  `money` int(10) NOT NULL,
   `created` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -81,8 +128,9 @@ CREATE TABLE `users` (
 -- Zrzut danych tabeli `users`
 --
 
-INSERT INTO `users` (`uid`, `login`, `password`, `money`, `created`) VALUES
-(1, 'admin', '$2y$10$GEO80edBKrYoDIEXiY0W3eT2w.aGZceklHYBHzcB/wu8K/0THSFR6', 30, '2021-11-29 21:00:27');
+INSERT INTO `users` (`uid`, `login`, `password`, `created`) VALUES
+(1, 'admin', '$2y$10$GEO80edBKrYoDIEXiY0W3eT2w.aGZceklHYBHzcB/wu8K/0THSFR6', '2021-11-29 21:00:27'),
+(2, 'user', '$2y$10$IF/moDXy49e23s/eAtlN3uYs1Aw9tAh/y4og9Z7ESD4s4sFg36fQS', '2021-12-09 20:59:58');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -93,6 +141,12 @@ INSERT INTO `users` (`uid`, `login`, `password`, `money`, `created`) VALUES
 --
 ALTER TABLE `skins`
   ADD UNIQUE KEY `skin` (`login`,`sid`) USING HASH;
+
+--
+-- Indeksy dla tabeli `statistics`
+--
+ALTER TABLE `statistics`
+  ADD UNIQUE KEY `login` (`login`) USING HASH;
 
 --
 -- Indeksy dla tabeli `users`
@@ -109,7 +163,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `uid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `uid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
